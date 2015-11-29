217333370948cc9abbffff9ad6c7ade9f50cba25
Jim Pelton <jimpelton@u.boisestate.edu>
2015-11-28 22:06:30 -0700

    Added empty RequestResponseSequenceAspect.

---
+++ app/src/main/java/com/morristaedt/mirror/aspect/RequestResponseSequenceAspect.java
@@ -0,0 +1,13 @@
+package com.morristaedt.mirror.aspect;
+
+import org.aspectj.lang.annotation.Aspect;
+
+/**
+ * Created by jim on 11/28/15.
+ */
+
+
+@Aspect
+public class RequestResponseSequenceAspect {
+
+}
